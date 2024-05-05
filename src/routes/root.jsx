c7e8d4252f90a620aa3bd22c562db50f49a82ffa
Nematou Ouedraogo <nemawest@Nemas-MacBook-Air.local>
2024-05-05 12:46:26 -0400

    Adding the search spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,6 +7,7 @@ import {
   Form,
   redirect,
   useNavigation,
+  useSubmit,
 } from "react-router-dom";
   import { getContacts, createContact } from "../contacts";
 
@@ -26,6 +27,12 @@ import {
 export default function Root() {
     const { contacts,q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+    const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
     useEffect(() => {
       document.getElementById("q").value = q;
     }, [q]);
@@ -37,16 +44,21 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
                 defaultValue={q}
+                onChange={(event) => {
+                  submit(event.currentTarget.form);
+                }}
               />
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
+               
               />
               <div
                 className="sr-only"
